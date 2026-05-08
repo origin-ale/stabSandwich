429475f6563992a3a6bfa2d65f88824a90fab8d0
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-05-08 17:23:41 +0200

    Change debugger to use definite times

--- Sandwich_debug.jl
+++ Sandwich_debug.jl
@@ -7,13 +7,9 @@ using ProgressMeter
 using Random
 using Revise
 
-# Random.seed!(42)
-
-# input_str = "XXXXXXXXXXXX"
-# χs = [4, 8, 16, 32, 64, 128]
-
-input_str = "ZZZZZZZ"
-χs = [4, 8, 16, 32, 64]
+N = 6
+input_str = join(rand(["I","X","Y","Z"], N))
+ss = collect(0:t)
 
 observable = stringtopauli(input_str)
 obs_string = pp.inttostring(observable.term, length(input_str))
@@ -24,50 +20,41 @@ t = Int(floor(2.5*N))
 paulistrings = [pp.inttosymbol(rand(0:4^N-1), N) for _ in 1:t]
 angles = 4π*rand(Float64, (t,)) # Rotation angles, i.e. 2* exponential phases
 
-# === TESTING ===
-# paulistrings = [[:Y, :Y, :Y],[:X, :Y, :Z]]
-# paulistrings = [[:Y, :X, :X],[:X, :Y, :Z]]
-# paulistrings = [[:X, :Y],[:X, :Z]]
-# angles = [π/4, π/4] # Rotation angles, i.e. 2* exponential phases
-# == ===
-
 qinds = [collect(1:N) for _ in paulistrings]
 rotations = pp.PauliRotation.(paulistrings, qinds)
 @assert t == length(rotations)
 
-for χ in χs
-  printstyled("DEBUGGING expectation value of $(N)-qubit string $obs_string through $t rotation layers with CAMPS bond dim $χ.\n"; color = :magenta)
-  # printstyled("DEBUGGING expectation value of $(N)-qubit string $obs_string through $t random rotation layers with CAMPS bond dim $χ.\n"; color = :magenta)
+printstyled(repeat("=",80), "\n\n"; color = :cyan)
+printstyled("DEBUGGING expectation value of $(N)-qubit string $obs_string through $t \
+  random rotation layers.\n"; color = :cyan)
+
+evs = []
+
+for s in ss
+  printstyled(repeat("-",16), " CAMPS until t = $s ", repeat("-",16), "\n"; color = :light_cyan)
   
   ψ = cmps.CAMPS(N)
-  ψ_evo, k, tstop = DisentangleCAMPS.evolve_bonddim(ψ, χ, rotations, angles; showprogress = true)
+  ψ_evo, k = DisentangleCAMPS.evolve(ψ, s, rotations, angles; showprogress = true)
 
-  leftover_rotations = rotations[tstop+1:end]
-  leftover_phases = angles[tstop+1:end]
-
-  if tstop == t
-    println("Maximum bond dimension $χ not reached before s=t=$(t).")
-  else
-    println("Maximum bond dimension $χ reached at s=$(tstop).")
-    println("Propagating Paulis over $(length(leftover_rotations)) gates")
-    # === Print leftover rotations ===
-    # for i in eachindex(leftover_rotations)
-    #    println(leftover_rotations[i].symbols, '\t', leftover_phases[i])
-    # end
-  end
+  leftover_rotations = rotations[s+1:end]
+  leftover_phases = angles[s+1:end]
 
   sandwichstrings = pp.propagate(leftover_rotations, observable, leftover_phases)
 
   println("Converting $(length(sandwichstrings))-term sum…")
   sandwichsum, conversiontime, _... = @timed cmps.PauliSum(sandwichstrings)
   println("Done in $conversiontime s.")
-  # === Show sums before & after conversion ===
-  # println("===== FROM =====\n", sandwichstrings)
-  # println("===== TO =====\n", sandwichsum)
   println("Computing expectation value of $(length(sandwichstrings))-term sum on CAMPS with bond dims $(ψ_evo.mps)…")
   ev, evtime, _... = @timed cmps.expectation(ψ_evo, sandwichsum)
   println("Done in $evtime s.")
   println("⟨$(obs_string)⟩ = $ev")
-  # println("Full MPS:\n", ψ_evo.mps.data)
   println("\n")
+  push!(evs, ev)
+end
+
+if all(isapprox.(evs[1], evs; atol=1e-10))
+  printstyled("All expectation values match.\n"; color = :green)
+else
+  printstyled("Expectation values DO NOT match:\n"; color = :red)
+  print(evs)
 end
