5010f8b64119af6016906d635141e38080533efe
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-05-11 11:29:41 +0200

    Update Sandwich_tomography.jl to use new functions

--- debugging/Sandwich_tomography.jl
+++ debugging/Sandwich_tomography.jl
@@ -4,23 +4,14 @@ import PauliPropagation as pp
 using QuantumClifford
 using DisentangleCAMPS
 using ProgressMeter
-using Random
-
-Random.seed!(42)
+using Revise
 
 χs = [1,2]
-t = 2
 N = 2
+
 paulistrings = [[:X, :Y], [:Y, :X]]
-# paulistrings = [[:I, :X], [:X, :Z], [:Y, :Z], [:I, :Z], [:X, :I],
-#                 [:Y, :Z], [:Y, :I], [:Y, :Y], [:I, :I], [:Z, :Z],
-#                 [:Z, :I], [:X, :X], [:Y, :X], [:Z, :I], [:Y, :X],
-#                 [:X, :Y], [:X, :Z], [:Z, :Z], [:X, :Z], [:X, :Y]]
-angles = fill(-π/2, t) # Rotation angles, i.e. 2* exponential phases
-# angles = [-π/2, -π/2] # Rotation angles, i.e. 2* exponential phases
-qinds = [collect(1:N) for _ in paulistrings]
-rotations = pp.PauliRotation.(paulistrings, qinds)
-@assert t == length(rotations)
+phases = fill(-π/2, length(paulistrings))
+rotations, phases = rotation_circuit(paulistrings, phases, N)
 
 ppres = []
 campsres = []
@@ -32,42 +23,28 @@ for input_str in input_strs
 
   for χ in χs
     printstyled("DEBUGGING expectation value of $(N)-qubit string $obs_string through $t rotation layers with CAMPS bond dim $χ.\n"; color = :magenta)
-    # printstyled("DEBUGGING expectation value of $(N)-qubit string $obs_string through $t random rotation layers with CAMPS bond dim $χ.\n"; color = :magenta)
     
     ψ = cmps.CAMPS(N)
-    ψ_evo, k, tstop = DisentangleCAMPS.evolve_bonddim(ψ, χ, rotations, angles; showprogress = true)
+    ψ_evo, k, tstop = DisentangleCAMPS.evolve_bonddim(ψ, χ, rotations, phases; showprogress = true)
 
-    leftover_rotations = rotations[tstop+1:end]
-    leftover_phases = angles[tstop+1:end]
+    leftover_rotations, leftover_angles = leftover_rotgates(tstop, rotations, phases)
 
     if tstop == t
       println("Maximum bond dimension $χ not reached before s=t=$(t).")
     else
       println("Maximum bond dimension $χ reached at s=$(tstop).")
       println("Propagating Paulis over $(length(leftover_rotations)) gates")
-      # === Print leftover rotations ===
-      # for i in eachindex(leftover_rotations)
-      #    println(leftover_rotations[i].symbols, '\t', leftover_phases[i])
-      # end
     end
 
-    sandwichstrings = pp.propagate(leftover_rotations, observable, leftover_phases)
-    # (χ == 1) && @show sandwichstrings
+    sandwichstrings = pp.propagate(leftover_rotations, observable, leftover_angles)
 
-    println("Converting $(length(sandwichstrings))-term sum…")
-    sandwichsum, conversiontime, _... = @timed cmps.PauliSum(sandwichstrings)
-    println("Done in $conversiontime s.")
-    # === Show sums before & after conversion ===
-    # println("===== FROM =====\n", sandwichstrings)
-    # println("===== TO =====\n", sandwichsum)
-    println("Computing expectation value of $(length(sandwichstrings))-term sum on CAMPS with bond dims $(ψ_evo.mps)…")
-    ev, evtime, _... = @timed cmps.expectation(ψ_evo, sandwichsum)
-    println("Done in $evtime s.")
+    ev = cmps.expectation(ψ, sandwichstrings; verbose = true)
     println("⟨$(obs_string)⟩ = $ev")
-    # println("Full MPS:\n", ψ_evo.mps.data)
     println("\n")
     (χ == 1) && push!(ppres, ev)
     (χ == 2) && push!(campsres, ev)
   end
 end
+
+println("Coincidence of traces:")
 println(isapprox.(ppres, campsres; atol = 1e-10))
