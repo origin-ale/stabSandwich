43d331900c16f6a30fa91747c19e9b859d9d2936
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-06-12 12:53:17 +0200

    Modernize fSim and XXZ circuit files

--- XXZ_circuit.jl
+++ XXZ_circuit.jl
@@ -1,12 +1,19 @@
 using Revise
 using CampsPP
 using DisentangleCAMPS
+using ITensors, ITensorMPS
 
 import PauliPropagation as pp
 import CliffordMPS as cmps
 
 using Printf
 using Random: seed!
+using Strided
+using LinearAlgebra
+
+Strided.disable_threads()
+BLAS.set_num_threads(1)
+ITensors.Strided.set_num_threads(1)
 
 seed!(2)
 
@@ -24,39 +31,48 @@ Nmax_pp = Nmax_campspp
 magic_prob = .5
 magic_syms = [:X]
 magic_inds = [N÷2]
-output = "output/XXZDynamics.txt"
-
-layer_depth = length(xxz_circuit(0,0,1,N)[1])
-layer_ends = collect(layer_depth:layer_depth:t*layer_depth)
+warn_on_prestop = true
 
 ψ = cmps.CAMPS(N)
 onebitinds = Integer[]
 Zs = [pp.PauliString(N, [:Z], [i], 1/N) for i = 1:N]
 obs = pp.PauliSum(Zs)
 
+layer_ends = layerends(N, t, xxz_circuit)
 gates, phases = xxz_circuit(ϕ, θ, t, N)
 gates, phases, layer_ends = dopeMagic(N, gates, phases, layer_ends, magic_syms, magic_inds, magic_prob)
 
-printstyled("Running magic-doped XXZ circuit dynamics until failure for N=$N, χ_campspp = $χ_campspp, Nmax_campspp = $Nmax_campspp.\n"; color = :cyan)
+output = "output/XXZDynamics_$(N).txt"
+param_info = Dict(
+  "N" => N,
+  "t" => t,
+  "ϕ" => ϕ,
+  "θ" => θ,
+  "magic_prob" => magic_prob,
+  "χ_campspp" => χ_campspp,
+  "thl_campspp" => thl_campspp,
+  "Nmax_campspp" => Nmax_campspp,
+  "χ_camps" => χ_camps,
+  "thl_pp" => thl_pp,
+  "Nmax_pp" => Nmax_pp,
+  "n.gates" => length(phases))
 
-CampsPP.initialize_output(
-  output, 
-  "Magnetization", 
-  Dict(
-    "N" => N, 
-    "χ" => χ_campspp, 
-    "Nmax" => Nmax_campspp))
+printstyled("Running magic-doped XXZ circuit dynamics until failure for N=$N, χ_campspp = $χ_campspp, thl_campspp = $thl_campspp, Nmax_campspp = $Nmax_campspp.\n"; color = :cyan)
 
-evs = campspp_circuit_dynamics(
-  ψ, χ_campspp, thl_campspp, Nmax_campspp, gates, phases, obs, output; 
+initialize_output(output, "magnetization", param_info)
+_, t_stop = campspp_circuit_dynamics(
+  ψ, χ_campspp, thl_campspp, Nmax_campspp, gates, phases, obs, output;
   showprogress = true, layer_ends = layer_ends)
+if warn_on_prestop && (t_stop < length(phases))
+  printstyled("WARNING: CAMPS+PP run stopped at gate $t_stop/$(length(phases))\n"; color = :yellow)
+end
 
 _ = camps_circuit_dynamics(
-  ψ, gates, phases, χ_camps, obs, output; 
+  ψ, gates, phases, χ_camps, obs, output;
   showprogress = true, layer_ends = layer_ends)
 
 _ = pauliprop_circuit_dynamics(
-  onebitinds, gates, phases, thl_pp, Nmax_pp, obs, output; 
+  onebitinds, gates, phases, thl_pp, Nmax_pp, obs, output;
   showprogress = true, layer_ends = layer_ends)
 
 return
--- fSim_circuit.jl
+++ fSim_circuit.jl
@@ -1,17 +1,27 @@
 using Revise
 using CampsPP
 using DisentangleCAMPS
+using ITensors, ITensorMPS
 
 import PauliPropagation as pp
 import CliffordMPS as cmps
 
 using Printf
 using Random: seed!
+using Strided
+using LinearAlgebra
+
+Strided.disable_threads()
+BLAS.set_num_threads(1)
+ITensors.Strided.set_num_threads(1)
 
 seed!(2)
 
 N = 10
 t = 100
+θ = π/2
+ϕ = π
+
 χ_campspp = 64
 thl_campspp = 1e-15
 Nmax_campspp = 200
@@ -19,33 +29,51 @@ Nmax_campspp = 200
 thl_pp = thl_campspp
 Nmax_pp = Nmax_campspp
 magic_prob = 0.5
+warn_on_prestop = true
 
 ψ = cmps.CAMPS(N)
 Zs = [pp.PauliString(N, [:Z], [i], 1/N) for i = 1:N]
 obs = pp.PauliSum(Zs)
-end_fSim = [:Z, :I]
 
-gates, phases = fSim_circuit(π/2, π, t, N)
-gates, phases, magic_pos = dopeT(N, gates, phases, magic_prob)
+layer_ends = layerends(N, t, fSim_circuit)
+gates, phases = fSim_circuit(θ, ϕ, t, N)
+n_undoped = length(gates)
+gates, phases, layer_ends = dopeT(N, gates, phases, layer_ends, magic_prob)
 
-output = "output/fSimDynamics.txt"
+output = "output/fSimDynamics_$(N).txt"
+param_info = Dict(
+  "N" => N,
+  "t" => t,
+  "θ" => θ,
+  "ϕ" => ϕ,
+  "magic_prob" => magic_prob,
+  "χ_campspp" => χ_campspp,
+  "thl_campspp" => thl_campspp,
+  "Nmax_campspp" => Nmax_campspp,
+  "χ_camps" => χ_camps,
+  "thl_pp" => thl_pp,
+  "Nmax_pp" => Nmax_pp,
+  "n.gates" => length(phases))
 
-printstyled("Running magic-doped fSim circuit dynamics until failure for N=$N, χ_campspp = $χ_campspp, Nmax_campspp = $Nmax_campspp.\n"; color = :cyan)
-println("Magic doping at $(length(magic_pos)) gates of $(length(gates)).")
+printstyled("Running magic-doped fSim circuit dynamics until failure for N=$N, χ_campspp = $χ_campspp, thl_campspp = $thl_campspp, Nmax_campspp = $Nmax_campspp.\n"; color = :cyan)
+println("Magic doping at $(length(gates) - n_undoped) gates of $(length(gates)).")
 
-CampsPP.initialize_output(output, "magnetization", Dict(:N => N, :χ => χ_campspp, :Nmax => Nmax_campspp))
-evs = campspp_circuit_dynamics(ψ, χ_campspp, thl_campspp, Nmax_campspp, gates, phases, obs, output; showprogress = true, ev_at = end_fSim)
-
-open(output, "a") do f
-  println(f, "\n")
+initialize_output(output, "magnetization", param_info)
+_, t_stop = campspp_circuit_dynamics(
+  ψ, χ_campspp, thl_campspp, Nmax_campspp, gates, phases, obs, output;
+  showprogress = true, layer_ends = layer_ends)
+if warn_on_prestop && (t_stop < length(phases))
+  printstyled("WARNING: CAMPS+PP run stopped at gate $t_stop/$(length(phases))\n"; color = :yellow)
 end
-_ = camps_circuit_dynamics(ψ, χ_camps, gates, phases, obs, output; showprogress = true, ev_at = end_fSim)
 
-angles = -2 .* phases
-open(output, "a") do f
-  println(f, "\n")
-end
+_ = camps_circuit_dynamics(
+  ψ, gates, phases, χ_camps, obs, output;
+  showprogress = true, layer_ends = layer_ends)
+
 ev = cmps.expectation(ψ, obs)
-CampsPP.append_datapoint(output, 0, real(ev))
-_ = pauliprop_circuit_dynamics(ψ, 0, thl_pp, gates, angles, Nmax_pp, obs, output; showprogress = true, ev_at = end_fSim)
+append_datapoint(output, 0, real(ev))
+_ = pauliprop_circuit_dynamics(
+  ψ, 0, gates, phases, thl_pp, Nmax_pp, obs, output;
+  showprogress = true, layer_ends = layer_ends)
+
 return
